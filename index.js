2b011e4a09f8704638db3fd5f9db3203a3025d51
Shreesha-NP <tareyfan11@gmail.com>
2019-12-20 15:28:46 +0530

    add

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('frontend'))
+
+app.get('/', (req, res) => res.sendFile(__dirname+'/frontend/html/home.html'))
+var student={"name":"being zero","college":"vnr","regno":98}
+app.get('/data',(req,res)=> res.json(student));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
